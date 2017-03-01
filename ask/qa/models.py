66f779a016450f95bdab74e9af82307e1e30e539
root <Andrtvr@yandex.ru>
2017-03-01 18:03:18 +0300

    add default x

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,6 +2,22 @@ from __future__ import unicode_literals
 from django.db import models, connection
 from django.contrib.auth.models import User, UserManager
 
+class QuestionManager(models.Manager):
+    def new(self):
+        cursor = connection.cursor()
+        cursor.execute(""" SELECT title,text FROM qa_question ORDER BY id DESC LIMIT 3 """)
+        result_list = []
+        for i in cursor.fetchall():
+            c = result_list.append(i)
+        return result_list
+
+    def popular(self):
+        cursor = connection.cursor()
+        cursor.execute(""" SELECT title, text FROM qa_question ORDER BY rating""")
+        result_list = []
+        for i in cursor.fetchall():
+            c = result_list.append(i)
+        return result_list
 
 class Question(models.Model):
     title = models.CharField(max_length=30)
@@ -10,6 +26,7 @@ class Question(models.Model):
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     likes = models.ManyToManyField(User, related_name='Likes')
+    QuestionManager = QuestionManager()
 
 class Answer(models.Model):
      text = models.CharField(max_length=200)
@@ -22,7 +39,7 @@ class Likes(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     inviter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Likes_invites')
     invite_reason = models.TextField()
-'''
+
 
 class QuestionManager(models.Manager):
     def new(self):
@@ -41,3 +58,4 @@ class QuestionManager(models.Manager):
             c = result_list.append(i)
         return result_list
 
+'''
