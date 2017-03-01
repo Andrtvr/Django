20c0d7fe07157cd512c843dcaac33770cc6436fa
root <Andrtvr@yandex.ru>
2017-03-01 15:56:28 +0300

    add list Likes1

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,14 +2,8 @@ from __future__ import unicode_literals
 from django.db import models, connection
 from django.contrib.auth.models import User, UserManager
 
-
-#class Users(User):
- #    objects = UserManager()
-
-
 class Likes(models.Model):
-    User_name = models.ForeignKey(User)
-
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Question(models.Model):
     title = models.CharField(max_length=30)
@@ -18,14 +12,20 @@ class Question(models.Model):
     rating = models.IntegerField('Rating')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(Likes)
-
+   # likes = models.ManyToManyField(User, through='Likes', through_fields=('user','question'))
 
 class Answer(models.Model):
      text = models.CharField(max_length=200)
      added_at = models.DateTimeField('Date published')
      question = models.TextField()
      author = models.TextField()
-
+"""
+class Likes(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    inviter = models.ForeignKey(User,on_delete=models.CASCADE)
+    invite_reason = models.CharField
+"""
 
 class QuestionManager(models.Manager):
     def new(self):
