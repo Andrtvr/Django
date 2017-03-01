98b3db3f0376e183efa07880b60c27f0fd76c5de
Andrtvr <Andrtvr@yandex.ru>
2017-03-01 22:51:13 +0300

    end

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -26,36 +26,11 @@ class Question(models.Model):
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     likes = models.ManyToManyField(User, related_name='Likes')
-    QuestionManager = QuestionManager()
+    QuestionMa = QuestionManager()
 
 class Answer(models.Model):
-     text = models.CharField(max_length=200)
+     text = models.textField(max_length=200)
      added_at = models.DateTimeField('Date published')
-     question = models.TextField()
-     author = models.TextField()
-'''
-class Likes(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    inviter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Likes_invites')
-    invite_reason = models.TextField()
+     question = models.ForeignKey(Question)
+     author = models.ForeignKey(User)
 
-
-class QuestionManager(models.Manager):
-    def new(self):
-        cursor = connection.cursor()
-        cursor.execute(""" SELECT title,text FROM qa_question ORDER BY id DESC LIMIT 3 """)
-        result_list = []
-        for i in cursor.fetchall():
-            c = result_list.append(i)
-        return result_list
-
-    def popular(self):
-        cursor = connection.cursor()
-        cursor.execute(""" SELECT title, text FROM qa_question ORDER BY rating""")
-        result_list = []
-        for i in cursor.fetchall():
-            c = result_list.append(i)
-        return result_list
-
-'''
