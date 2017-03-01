47b861d474d1f65d8b83c0229bc7fc2ba8c332f1
root <Andrtvr@yandex.ru>
2017-03-01 16:40:37 +0300

    manyToMany

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,30 +2,27 @@ from __future__ import unicode_literals
 from django.db import models, connection
 from django.contrib.auth.models import User, UserManager
 
-class Likes(models.Model):
-    name = models.ForeignKey(User, on_delete=models.CASCADE)
 
 class Question(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.TextField(max_length=30)
     text = models.TextField(max_length=200)
     added_at = models.DateTimeField('Date published')
     rating = models.IntegerField('Rating')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(Likes)
-   # likes = models.ManyToManyField(User, through='Likes', through_fields=('user','question'))
+    likes = models.ManyToManyField(User, related_name='Likes')
 
 class Answer(models.Model):
      text = models.CharField(max_length=200)
      added_at = models.DateTimeField('Date published')
      question = models.TextField()
      author = models.TextField()
-"""
+'''
 class Likes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    inviter = models.ForeignKey(User,on_delete=models.CASCADE)
-    invite_reason = models.CharField
-"""
+    inviter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='Likes_invites')
+    invite_reason = models.TextField()
+'''
 
 class QuestionManager(models.Manager):
     def new(self):
